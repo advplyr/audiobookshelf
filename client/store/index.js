a19b6370c440d75dcf4e2490c3dddc2f370ddb7f
advplyr <advplyr@protonmail.com>
2022-05-08 18:40:43 -0500

    Fix:getBookCoverAspectRatio

--- client/store/index.js
+++ client/store/index.js
@@ -33,7 +33,7 @@ export const getters = {
     return state.serverSettings[key]
   },
   getBookCoverAspectRatio: state => {
-    if (!state.serverSettings || !state.serverSettings.coverAspectRatio) return 1
+    if (!state.serverSettings || isNaN(state.serverSettings.coverAspectRatio)) return 1
     return state.serverSettings.coverAspectRatio === 0 ? 1.6 : 1
   },
   getNumLibraryItemsSelected: state => state.selectedLibraryItems.length,
