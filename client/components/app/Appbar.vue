f51a31c8ca08ca6b647e4d60c923c6199c6c3919
advplyr <advplyr@protonmail.com>
2022-05-09 15:12:55 -0500

    Update:Remove back arrow in appbar

--- client/components/app/Appbar.vue
+++ client/components/app/Appbar.vue
@@ -2,11 +2,13 @@
   <div class="w-full h-16 bg-primary relative">
     <div id="appbar" class="absolute top-0 bottom-0 left-0 w-full h-full px-2 md:px-6 py-1 z-50">
       <div class="flex h-full items-center">
-        <img v-if="!showBack" src="/icon48.png" class="w-10 h-10 md:w-12 md:h-12 mr-4" />
-        <a v-if="showBack" @click="back" class="rounded-full h-12 w-12 flex items-center justify-center hover:bg-white hover:bg-opacity-10 mr-4 cursor-pointer">
-          <span class="material-icons text-4xl text-white">arrow_back</span>
-        </a>
-        <h1 class="text-2xl font-book mr-6 hidden lg:block">audiobookshelf</h1>
+        <nuxt-link to="/">
+          <img src="/icon48.png" class="w-10 h-10 md:w-12 md:h-12 mr-4" />
+        </nuxt-link>
+
+        <nuxt-link to="/">
+          <h1 class="text-2xl font-book mr-6 hidden lg:block hover:underline">audiobookshelf</h1>
+        </nuxt-link>
 
         <ui-libraries-dropdown />
 
@@ -93,9 +95,6 @@ export default {
     },
     isHome() {
       return this.$route.name === 'library-library'
-    },
-    showBack() {
-      return this.$route.name !== 'library-library-bookshelf-id' && !this.isHome
     },
     user() {
       return this.$store.state.user.user
