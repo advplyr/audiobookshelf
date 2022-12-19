6dc4dc8f4926788a4dbff1f1e42e131993e02887
Scott Ruoti <ruoti@utk.edu>
2022-12-19 10:07:40 -0500

    Updating devcontainer setup and related documentation

---
+++ .devcontainer/dev.js
@@ -0,0 +1,9 @@
+// Using port 3333 is important when running the client web app separately
+const Path = require('path')
+module.exports.config = {
+  Port: 3333,
+  ConfigPath: Path.resolve('config'),
+  MetadataPath: Path.resolve('metadata'),
+  FFmpegPath: '/usr/bin/ffmpeg',
+  FFProbePath: '/usr/bin/ffprobe'
+}
