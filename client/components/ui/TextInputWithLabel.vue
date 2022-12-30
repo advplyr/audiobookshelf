5a3f14ae51127b844daf5a34f4f9397803610d40
advplyr <advplyr@protonmail.com>
2022-12-29 18:03:05 -0600

    Remove extra space from label

--- client/components/ui/TextInputWithLabel.vue
+++ client/components/ui/TextInputWithLabel.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full">
     <slot>
-      <label :for="identifier" class="px-1 text-sm font-semibold" :class="{ 'text-gray-400': disabled }">
-        {{ label }}<em v-if="note" class="font-normal text-xs pl-2">{{ note }}</em>
-      </label>
+      <label :for="identifier" class="px-1 text-sm font-semibold" :class="{ 'text-gray-400': disabled }"
+        >{{ label }}<em v-if="note" class="font-normal text-xs pl-2">{{ note }}</em></label
+      >
     </slot>
     <ui-text-input :placeholder="label" :inputId="identifier" ref="input" v-model="inputValue" :disabled="disabled" :readonly="readonly" :type="type" class="w-full" :class="inputClass" @blur="inputBlurred" />
   </div>
