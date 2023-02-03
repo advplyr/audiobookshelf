8c4d3b93c8f4ed26a1831a97bda48a604b7b33b3
Tomazed <toma.zed@gmail.com>
2023-02-03 11:25:02 +0100

    revert formatting

--- client/pages/config/item-metadata-utils/index.vue
+++ client/pages/config/item-metadata-utils/index.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
     <app-settings-content :header-text="$strings.HeaderItemMetadataUtils">
-      <nuxt-link to="/config/item-metadata-utils/tags"
-        class="block w-full rounded bg-primary/40 hover:bg-primary/60 text-gray-300 hover:text-white p-4 mt-6 mb-2">
+      <nuxt-link to="/config/item-metadata-utils/tags" class="block w-full rounded bg-primary/40 hover:bg-primary/60 text-gray-300 hover:text-white p-4 mt-6 mb-2">
         <div class="flex justify-between">
           <p>{{ $strings.HeaderManageTags }}</p>
           <span class="material-icons">arrow_forward</span>
         </div>
       </nuxt-link>
-      <nuxt-link to="/config/item-metadata-utils/genres"
-        class="block w-full rounded bg-primary/40 hover:bg-primary/60 text-gray-300 hover:text-white p-4 my-2">
+      <nuxt-link to="/config/item-metadata-utils/genres" class="block w-full rounded bg-primary/40 hover:bg-primary/60 text-gray-300 hover:text-white p-4 my-2">
         <div class="flex justify-between">
           <p>{{ $strings.HeaderManageGenres }}</p>
           <span class="material-icons">arrow_forward</span>
@@ -27,11 +25,11 @@ export default {
   watch: {},
   computed: {},
   methods: {
-    init() { }
+    init() {}
   },
   mounted() {
     this.init()
   },
-  beforeDestroy() { }
+  beforeDestroy() {}
 }
 </script>
