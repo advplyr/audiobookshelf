a99257e758864837bb09f0bebd5298ec29735519
advplyr <advplyr@protonmail.com>
2023-03-25 14:07:35 -0500

    Fix getAllLibraryItemsInProgress route

--- server/controllers/MeController.js
+++ server/controllers/MeController.js
@@ -256,13 +256,13 @@ class MeController {
   }
 
   // GET: api/me/items-in-progress
-  async getAllLibraryItemsInProgress(req, res) {
+  getAllLibraryItemsInProgress(req, res) {
     const limit = !isNaN(req.query.limit) ? Number(req.query.limit) || 25 : 25
 
-    var itemsInProgress = []
+    let itemsInProgress = []
     for (const mediaProgress of req.user.mediaProgress) {
-      if (!mediaProgress.isFinished && (mediaProgress.progress > 0 || libraryItem.ebookProgress > 0)) {
-        const libraryItem = await this.db.getLibraryItem(mediaProgress.libraryItemId)
+      if (!mediaProgress.isFinished && (mediaProgress.progress > 0 || mediaProgress.ebookProgress > 0)) {
+        const libraryItem = this.db.getLibraryItem(mediaProgress.libraryItemId)
         if (libraryItem) {
           if (mediaProgress.episodeId && libraryItem.mediaType === 'podcast') {
             const episode = libraryItem.media.episodes.find(ep => ep.id === mediaProgress.episodeId)
