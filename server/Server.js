5a01be1ee31a47be1390f59c9c23bcb1bab3cbcf
advplyr <advplyr@protonmail.com>
2023-06-06 15:40:52 -0500

    Add tempFileDir for uploads

--- server/Server.js
+++ server/Server.js
@@ -152,6 +152,7 @@ class Server {
       defCharset: 'utf8',
       defParamCharset: 'utf8',
       useTempFiles: true,
+      tempFileDir: Path.join(global.MetadataPath, 'tmp')
     }))
     router.use(express.urlencoded({ extended: true, limit: "5mb" }));
     router.use(express.json({ limit: "5mb" }))
