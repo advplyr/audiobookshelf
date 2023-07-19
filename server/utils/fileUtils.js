38596d017f842ba64dd1cfef64ce9623ea04cd36
Rasmus Krämer <git@rfk.io>
2023-07-19 23:59:00 +0200

    Fix byte conversion

--- server/utils/fileUtils.js
+++ server/utils/fileUtils.js
@@ -83,7 +83,7 @@ function bytesPretty(bytes, decimals = 0) {
   if (bytes === 0) {
     return '0 Bytes'
   }
-  const k = 1024
+  const k = 1000
   var dm = decimals < 0 ? 0 : decimals
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   const i = Math.floor(Math.log(bytes) / Math.log(k))
