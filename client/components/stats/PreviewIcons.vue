15f83986e74073cfb7f07162d23fa6578120b71d
advplyr <advplyr@protonmail.com>
2024-04-18 17:45:47 -0500

    Update library stats previewicons padding

--- client/components/stats/PreviewIcons.vue
+++ client/components/stats/PreviewIcons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-wrap justify-between mt-6 px-8">
+  <div class="flex flex-wrap justify-center mt-6">
     <div class="flex p-2">
       <svg class="h-14 w-14" viewBox="0 0 24 24">
         <path fill="currentColor" d="M9 3V18H12V3H9M12 5L16 18L19 17L15 4L12 5M5 5V18H8V5H5M3 19V21H21V19H3Z" />
