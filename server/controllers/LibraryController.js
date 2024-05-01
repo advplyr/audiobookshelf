7229cfce84ada9e9500523344cdd7a63cdf47d84
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2024-05-01 07:20:48 -0400

    Added limit 10 to getAuthorsWithCount() call
    As per nichwall's request

--- server/controllers/LibraryController.js
+++ server/controllers/LibraryController.js
@@ -605,7 +605,7 @@ class LibraryController {
     }
 
     if (req.library.isBook) {
-      const authors = await authorFilters.getAuthorsWithCount(req.library.id)
+      const authors = await authorFilters.getAuthorsWithCount(req.library.id, 10)
       const genres = await libraryItemsBookFilters.getGenresWithCount(req.library.id)
       const bookStats = await libraryItemsBookFilters.getBookLibraryStats(req.library.id)
       const longestBooks = await libraryItemsBookFilters.getLongestBooks(req.library.id, 10)
