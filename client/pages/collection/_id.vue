b35fabbe55146f7d2a721a4bbfea51cb3f5c0ad4
advplyr <advplyr@protonmail.com>
2024-09-14 16:04:50 -0500

    Update:Collection & playlist Play button renamed to Play All #3320

--- client/pages/collection/_id.vue
+++ client/pages/collection/_id.vue
@@ -16,7 +16,7 @@
 
             <ui-btn v-if="showPlayButton" :disabled="streaming" color="success" :padding-x="4" small class="flex items-center h-9 mr-2" @click="clickPlay">
               <span v-show="!streaming" class="material-symbols fill text-2xl -ml-2 pr-1 text-white">play_arrow</span>
-              {{ streaming ? $strings.ButtonPlaying : $strings.ButtonPlay }}
+              {{ streaming ? $strings.ButtonPlaying : $strings.ButtonPlayAll }}
             </ui-btn>
 
             <!-- RSS feed -->
