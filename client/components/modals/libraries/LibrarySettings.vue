6ca277a21d6e0dd4c9159ecfeaff159e53635b90
advplyr <advplyr@protonmail.com>
2024-10-23 17:11:41 -0500

    Update:Library settings tab settings in 2 columns and cleanup

--- client/components/modals/libraries/LibrarySettings.vue
+++ client/components/modals/libraries/LibrarySettings.vue
@@ -1,78 +1,80 @@
 <template>
   <div class="w-full h-full px-1 md:px-4 py-1 mb-4">
-    <div class="flex items-center py-3">
-      <ui-toggle-switch v-model="useSquareBookCovers" @input="formUpdated" />
-      <ui-tooltip :text="$strings.LabelSettingsSquareBookCoversHelp">
-        <p class="pl-4 text-base">
-          {{ $strings.LabelSettingsSquareBookCovers }}
-          <span class="material-symbols icon-text text-sm">info</span>
-        </p>
-      </ui-tooltip>
-    </div>
-    <div class="py-3">
-      <div class="flex items-center">
-        <ui-toggle-switch v-if="!globalWatcherDisabled" v-model="enableWatcher" @input="formUpdated" />
-        <ui-toggle-switch v-else disabled :value="false" />
-        <p class="pl-4 text-base">{{ $strings.LabelSettingsEnableWatcherForLibrary }}</p>
-      </div>
-      <p v-if="globalWatcherDisabled" class="text-xs text-warning">*{{ $strings.MessageWatcherIsDisabledGlobally }}</p>
-    </div>
-    <div v-if="isBookLibrary" class="flex items-center py-3">
-      <ui-toggle-switch v-model="audiobooksOnly" @input="formUpdated" />
-      <ui-tooltip :text="$strings.LabelSettingsAudiobooksOnlyHelp">
-        <p class="pl-4 text-base">
-          {{ $strings.LabelSettingsAudiobooksOnly }}
-          <span class="material-symbols icon-text text-sm">info</span>
-        </p>
-      </ui-tooltip>
-    </div>
-    <div v-if="isBookLibrary" class="py-3">
-      <div class="flex items-center">
-        <ui-toggle-switch v-model="skipMatchingMediaWithAsin" @input="formUpdated" />
-        <p class="pl-4 text-base">{{ $strings.LabelSettingsSkipMatchingBooksWithASIN }}</p>
-      </div>
-    </div>
-    <div v-if="isBookLibrary" class="py-3">
-      <div class="flex items-center">
-        <ui-toggle-switch v-model="skipMatchingMediaWithIsbn" @input="formUpdated" />
-        <p class="pl-4 text-base">{{ $strings.LabelSettingsSkipMatchingBooksWithISBN }}</p>
-      </div>
-    </div>
-    <div v-if="isBookLibrary" class="py-3">
-      <div class="flex items-center">
-        <ui-toggle-switch v-model="hideSingleBookSeries" @input="formUpdated" />
-        <ui-tooltip :text="$strings.LabelSettingsHideSingleBookSeriesHelp">
-          <p class="pl-4 text-base">
-            {{ $strings.LabelSettingsHideSingleBookSeries }}
+    <div class="flex flex-wrap">
+      <div class="flex items-center p-2 w-full md:w-1/2">
+        <ui-toggle-switch v-model="useSquareBookCovers" size="sm" @input="formUpdated" />
+        <ui-tooltip :text="$strings.LabelSettingsSquareBookCoversHelp">
+          <p class="pl-4 text-sm">
+            {{ $strings.LabelSettingsSquareBookCovers }}
             <span class="material-symbols icon-text text-sm">info</span>
           </p>
         </ui-tooltip>
       </div>
-    </div>
-    <div v-if="isBookLibrary" class="py-3">
-      <div class="flex items-center">
-        <ui-toggle-switch v-model="onlyShowLaterBooksInContinueSeries" @input="formUpdated" />
-        <ui-tooltip :text="$strings.LabelSettingsOnlyShowLaterBooksInContinueSeriesHelp">
-          <p class="pl-4 text-base">
-            {{ $strings.LabelSettingsOnlyShowLaterBooksInContinueSeries }}
+      <div class="p-2 w-full md:w-1/2">
+        <div class="flex items-center">
+          <ui-toggle-switch v-if="!globalWatcherDisabled" v-model="enableWatcher" size="sm" @input="formUpdated" />
+          <ui-toggle-switch v-else disabled size="sm" :value="false" />
+          <p class="pl-4 text-sm">{{ $strings.LabelSettingsEnableWatcherForLibrary }}</p>
+        </div>
+        <p v-if="globalWatcherDisabled" class="text-xs text-warning">*{{ $strings.MessageWatcherIsDisabledGlobally }}</p>
+      </div>
+      <div v-if="isBookLibrary" class="flex items-center p-2 w-full md:w-1/2">
+        <ui-toggle-switch v-model="audiobooksOnly" size="sm" @input="formUpdated" />
+        <ui-tooltip :text="$strings.LabelSettingsAudiobooksOnlyHelp">
+          <p class="pl-4 text-sm">
+            {{ $strings.LabelSettingsAudiobooksOnly }}
             <span class="material-symbols icon-text text-sm">info</span>
           </p>
         </ui-tooltip>
       </div>
-    </div>
-    <div v-if="isBookLibrary" class="py-3">
-      <div class="flex items-center">
-        <ui-toggle-switch v-model="epubsAllowScriptedContent" @input="formUpdated" />
-        <ui-tooltip :text="$strings.LabelSettingsEpubsAllowScriptedContentHelp">
-          <p class="pl-4 text-base">
-            {{ $strings.LabelSettingsEpubsAllowScriptedContent }}
-            <span class="material-symbols icon-text text-sm">info</span>
-          </p>
-        </ui-tooltip>
+      <div v-if="isBookLibrary" class="p-2 w-full md:w-1/2">
+        <div class="flex items-center">
+          <ui-toggle-switch v-model="skipMatchingMediaWithAsin" size="sm" @input="formUpdated" />
+          <p class="pl-4 text-sm">{{ $strings.LabelSettingsSkipMatchingBooksWithASIN }}</p>
+        </div>
       </div>
-    </div>
-    <div v-if="isPodcastLibrary" class="py-3">
-      <ui-dropdown :label="$strings.LabelPodcastSearchRegion" v-model="podcastSearchRegion" :items="$podcastSearchRegionOptions" small class="max-w-72" menu-max-height="200px" @input="formUpdated" />
+      <div v-if="isBookLibrary" class="p-2 w-full md:w-1/2">
+        <div class="flex items-center">
+          <ui-toggle-switch v-model="skipMatchingMediaWithIsbn" size="sm" @input="formUpdated" />
+          <p class="pl-4 text-sm">{{ $strings.LabelSettingsSkipMatchingBooksWithISBN }}</p>
+        </div>
+      </div>
+      <div v-if="isBookLibrary" class="p-2 w-full md:w-1/2">
+        <div class="flex items-center">
+          <ui-toggle-switch v-model="hideSingleBookSeries" size="sm" @input="formUpdated" />
+          <ui-tooltip :text="$strings.LabelSettingsHideSingleBookSeriesHelp">
+            <p class="pl-4 text-sm">
+              {{ $strings.LabelSettingsHideSingleBookSeries }}
+              <span class="material-symbols icon-text text-sm">info</span>
+            </p>
+          </ui-tooltip>
+        </div>
+      </div>
+      <div v-if="isBookLibrary" class="p-2 w-full md:w-1/2">
+        <div class="flex items-center">
+          <ui-toggle-switch v-model="onlyShowLaterBooksInContinueSeries" size="sm" @input="formUpdated" />
+          <ui-tooltip :text="$strings.LabelSettingsOnlyShowLaterBooksInContinueSeriesHelp">
+            <p class="pl-4 text-sm">
+              {{ $strings.LabelSettingsOnlyShowLaterBooksInContinueSeries }}
+              <span class="material-symbols icon-text text-sm">info</span>
+            </p>
+          </ui-tooltip>
+        </div>
+      </div>
+      <div v-if="isBookLibrary" class="p-2 w-full md:w-1/2">
+        <div class="flex items-center">
+          <ui-toggle-switch v-model="epubsAllowScriptedContent" size="sm" @input="formUpdated" />
+          <ui-tooltip :text="$strings.LabelSettingsEpubsAllowScriptedContentHelp">
+            <p class="pl-4 text-sm">
+              {{ $strings.LabelSettingsEpubsAllowScriptedContent }}
+              <span class="material-symbols icon-text text-sm">info</span>
+            </p>
+          </ui-tooltip>
+        </div>
+      </div>
+      <div v-if="isPodcastLibrary" class="p-2 w-full md:w-1/2">
+        <ui-dropdown :label="$strings.LabelPodcastSearchRegion" v-model="podcastSearchRegion" :items="$podcastSearchRegionOptions" small class="max-w-72" menu-max-height="200px" @input="formUpdated" />
+      </div>
     </div>
   </div>
 </template>
