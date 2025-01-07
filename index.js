d410b13c9bec41ea9e0d459399325b13a3b1128d
mikiher <mikiher@gmail.com>
2025-01-07 17:41:09 +0200

    Merge prod.js into index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const optionDefinitions = [
+  { name: 'config', alias: 'c', type: String },
+  { name: 'metadata', alias: 'm', type: String },
+  { name: 'port', alias: 'p', type: String },
+  { name: 'host', alias: 'h', type: String },
+  { name: 'source', alias: 's', type: String },
+  { name: 'dev', alias: 'd', type: Boolean }
+]
+
+const commandLineArgs = require('./server/libs/commandLineArgs')
+const options = commandLineArgs(optionDefinitions)
+
+const Path = require('path')
+process.env.NODE_ENV = options.dev ? 'development' : process.env.NODE_ENV || 'production'
+
 const server = require('./server/Server')
 global.appRoot = __dirname
 
@@ -17,14 +32,19 @@ if (isDev) {
   process.env.ROUTER_BASE_PATH = devEnv.RouterBasePath || ''
 }
 
-const PORT = process.env.PORT || 80
-const HOST = process.env.HOST
-const CONFIG_PATH = process.env.CONFIG_PATH || '/config'
-const METADATA_PATH = process.env.METADATA_PATH || '/metadata'
-const SOURCE = process.env.SOURCE || 'docker'
+const inputConfig = options.config ? Path.resolve(options.config) : null
+const inputMetadata = options.metadata ? Path.resolve(options.metadata) : null
+
+const PORT = options.port || process.env.PORT || 3333
+const HOST = options.host || process.env.HOST
+const CONFIG_PATH = inputConfig || process.env.CONFIG_PATH || Path.resolve('config')
+const METADATA_PATH = inputMetadata || process.env.METADATA_PATH || Path.resolve('metadata')
+const SOURCE = options.source || process.env.SOURCE || 'debian'
+
 const ROUTER_BASE_PATH = process.env.ROUTER_BASE_PATH || ''
 
-console.log('Config', CONFIG_PATH, METADATA_PATH)
+console.log(`Running in ${process.env.NODE_ENV} mode.`)
+console.log(`Options: CONFIG_PATH=${CONFIG_PATH}, METADATA_PATH=${METADATA_PATH}, PORT=${PORT}, HOST=${HOST}, SOURCE=${SOURCE}, ROUTER_BASE_PATH=${ROUTER_BASE_PATH}`)
 
 const Server = new server(SOURCE, PORT, HOST, CONFIG_PATH, METADATA_PATH, ROUTER_BASE_PATH)
 Server.start()
