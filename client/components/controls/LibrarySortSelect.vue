7d8b857c771940cdd0294a6f852abc60f0576d0b
advplyr <advplyr@protonmail.com>
2025-07-28 14:58:28 -0400

    Add book library sort by progress updated #1215

--- client/components/controls/LibrarySortSelect.vue
+++ client/components/controls/LibrarySortSelect.vue
@@ -131,6 +131,10 @@ export default {
           value: 'mtimeMs'
         },
         {
+          text: this.$strings.LabelLibrarySortByProgress,
+          value: 'progress'
+        },
+        {
           text: this.$strings.LabelRandomly,
           value: 'random'
         }
