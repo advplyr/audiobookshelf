656936378eb493a478513583ee0d76129667c424
tagmeh <tagmeh@gmail.com>
2025-08-10 18:22:12 -0500

    Removed Cover.cy.js

--- client/cypress/tests/components/modals/item/tabs/Cover.cy.js
+++
@@ -1,8 +0,0 @@
-import Cover from '@/components/modals/item/tabs/Cover.vu'
-
-describe('Cover', () => {
-  it('should render', () => {
-    // Still working the tests out.
-    cy.mount(Cover)
-  })
-})
