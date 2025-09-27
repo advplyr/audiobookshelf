9ce6de3100bb2e0db1354f08c00c45085bcbb9d0
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2025-09-27 17:00:57 +0200

    Added progress to upload

--- client/components/ui/LoadingIndicator.vue
+++ client/components/ui/LoadingIndicator.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-40">
+  <div :class="hasSlotContent ? 'w-auto' : 'w-40'">
     <div class="bg-bg border border-gray-500 py-2 px-5 rounded-lg flex items-center flex-col box-shadow-md">
       <div class="loader-dots block relative w-20 h-5 mt-2">
         <div class="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
@@ -7,7 +7,9 @@
         <div class="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
         <div class="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
       </div>
-      <div class="text-gray-200 text-xs font-light mt-2 text-center">{{ message }}</div>
+      <slot>
+        <div class="text-gray-200 text-xs font-light mt-2 text-center">{{ message }}</div>
+      </slot>
     </div>
   </div>
 </template>
@@ -23,6 +25,9 @@ export default {
   computed: {
     message() {
       return this.text || this.$strings.MessagePleaseWait
+    },
+    hasSlotContent() {
+      return this.$slots.default && this.$slots.default.length > 0
     }
   }
 }
