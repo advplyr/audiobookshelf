816a47a4bac71ed4415e4923e99980a0150ce049
mikiher <mikiher@gmail.com>
2025-10-19 11:40:40 +0300

    Remove custom providers from library fetch action

--- client/store/libraries.js
+++ client/store/libraries.js
@@ -117,7 +117,6 @@ export const actions = {
         const library = data.library
         const filterData = data.filterdata
         const issues = data.issues || 0
-        const customMetadataProviders = data.customMetadataProviders || []
         const numUserPlaylists = data.numUserPlaylists
 
         dispatch('user/checkUpdateLibrarySortFilter', library.mediaType, { root: true })
