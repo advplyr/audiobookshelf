45e1b25ea3a1a85f18cea88f2d013900c4f53725
mikiher <mikiher@gmail.com>
2026-02-21 07:46:23 +0200

    AuthorController: Update author name in match if changed

--- server/controllers/AuthorController.js
+++ server/controllers/AuthorController.js
@@ -368,6 +368,11 @@ class AuthorController {
       hasUpdates = true
     }
 
+    if (authorData.name && req.author.name !== authorData.name) {
+      req.author.name = authorData.name
+      hasUpdates = true
+    }
+
     if (hasUpdates) {
       await req.author.save()
 
