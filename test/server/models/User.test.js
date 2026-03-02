fe834420cf9c73b482ccda51a9dc5c33c692bddc
Kevin Gatera <kevingatera@gmail.com>
2026-03-02 18:02:36 -0500

    fix postgres old user id json lookup

--- test/server/models/User.test.js
+++ test/server/models/User.test.js
@@ -1,7 +1,5 @@
 const { expect } = require('chai')
 const sinon = require('sinon')
-const sequelize = require('sequelize')
-
 const User = require('../../../server/models/User')
 
 describe('User model', () => {
@@ -17,9 +15,49 @@ describe('User model', () => {
       sinon.restore()
     })
 
-    it('should cast id to text on postgres when checking legacy token ids', async () => {
+    it('should resolve UUID ids via primary-key lookup on postgres', async () => {
       User.sequelize = {
         getDialect: () => 'postgres',
+        models: {
+          mediaProgress: {}
+        }
+      }
+
+      const user = { id: 'e8e677b2-da16-4220-ab67-443b7714caf9' }
+      const findByPkStub = sinon.stub(User, 'findByPk').resolves(user)
+      const findOneStub = sinon.stub(User, 'findOne').resolves(null)
+
+      const result = await User.getUserByIdOrOldId('e8e677b2-da16-4220-ab67-443b7714caf9')
+
+      expect(result).to.equal(user)
+      expect(findByPkStub.calledOnce).to.equal(true)
+      expect(findOneStub.called).to.equal(false)
+    })
+
+    it('should query legacy oldUserId with postgres-safe JSON matcher', async () => {
+      User.sequelize = {
+        getDialect: () => 'postgres',
+        models: {
+          mediaProgress: {}
+        }
+      }
+
+      const findByPkStub = sinon.stub(User, 'findByPk').resolves(null)
+      const findOneStub = sinon.stub(User, 'findOne').resolves(null)
+
+      await User.getUserByIdOrOldId('root')
+
+      expect(findByPkStub.called).to.equal(false)
+      expect(findOneStub.calledOnce).to.equal(true)
+
+      const options = findOneStub.firstCall.args[0]
+      expect(options.where.attribute.val).to.equal('"extraData"#>>\'{oldUserId}\'')
+      expect(options.where.logic).to.equal('root')
+    })
+
+    it('should keep sqlite oldUserId matcher unchanged', async () => {
+      User.sequelize = {
+        getDialect: () => 'sqlite',
         models: {
           mediaProgress: {}
         }
@@ -32,11 +70,7 @@ describe('User model', () => {
       expect(findOneStub.calledOnce).to.equal(true)
 
       const options = findOneStub.firstCall.args[0]
-      const whereOr = options.where[sequelize.Op.or]
-
-      expect(whereOr).to.have.length(2)
-      expect(whereOr[0].attribute.type).to.equal('text')
-      expect(whereOr[1]).to.deep.equal({ 'extraData.oldUserId': 'root' })
+      expect(options.where).to.deep.equal({ 'extraData.oldUserId': 'root' })
     })
   })
 })
