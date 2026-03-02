b580ef78c4e67a19ac8b0584ef74aa2a1d0cdb54
Kevin Gatera <kevingatera@gmail.com>
2026-03-02 17:07:10 -0500

    disable identifier quoting for postgres compatibility

--- server/Database.js
+++ server/Database.js
@@ -296,7 +296,8 @@ class Database {
       this.sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
         logging: logging,
-        benchmark: benchmark
+        benchmark: benchmark,
+        quoteIdentifiers: false
       })
     }
 
