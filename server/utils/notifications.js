b5feacb28f9a0151d912ab4b257cf798391ae265
Ben <bensycamore@gmail.com>
2026-08-01 11:11:45 -0600

    Add notification for manually downloaded podcast episodes

--- server/utils/notifications.js
+++ server/utils/notifications.js
@@ -29,6 +29,32 @@ module.exports.notificationData = {
       }
     },
     {
+      name: 'onPodcastEpisodeManuallyDownloaded',
+      requiresLibrary: true,
+      libraryMediaType: 'podcast',
+      description: 'Triggered when a podcast episode is manually downloaded',
+      descriptionKey: 'NotificationOnEpisodeManuallyDownloadedDescription',
+      variables: ['libraryItemId', 'libraryId', 'podcastTitle', 'podcastAuthor', 'podcastDescription', 'podcastGenres', 'episodeTitle', 'episodeSubtitle', 'episodeDescription', 'libraryName', 'episodeId', 'mediaTags'],
+      defaults: {
+        title: 'New {{podcastTitle}} Episode!',
+        body: '{{episodeTitle}} has been added to {{libraryName}} library.'
+      },
+      testData: {
+        libraryItemId: 'li_notification_test',
+        libraryId: 'lib_test',
+        libraryName: 'Podcasts',
+        mediaTags: 'TestTag1, TestTag2',
+        podcastTitle: 'Abs Test Podcast',
+        podcastAuthor: 'Audiobookshelf',
+        podcastDescription: 'Description of the Abs Test Podcast belongs here.',
+        podcastGenres: 'TestGenre1, TestGenre2',
+        episodeId: 'ep_notification_test',
+        episodeTitle: 'Successful Test Episode',
+        episodeSubtitle: 'Episode Subtitle',
+        episodeDescription: 'Some description of the podcast episode.'
+      }
+    },
+    {
       name: 'onBackupCompleted',
       requiresLibrary: false,
       description: 'Triggered when a backup is completed',
