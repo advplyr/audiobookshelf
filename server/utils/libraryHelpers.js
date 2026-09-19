2f75d8f5a9a8c8c272fb617f453256cc447a1c01
blue219 <lantianisbluesky@gmail.com>
2026-09-20 01:40:52 +1200

    fix: map legacy series sort fields to book properties

--- server/utils/libraryHelpers.js
+++ server/utils/libraryHelpers.js
@@ -151,6 +151,12 @@ module.exports = {
     }
 
     const sortingIgnorePrefix = Database.serverSettings.sortingIgnorePrefix
+    // Legacy API sort paths map to direct properties on the current Book model.
+    const legacyMediaSortFields = {
+      'media.metadata.authorName': 'authorName',
+      'media.metadata.authorNameLF': 'authorNameLF',
+      'media.metadata.publishedYear': 'publishedYear'
+    }
 
     let sortArray = []
     const direction = payload.sortDesc ? 'desc' : 'asc'
@@ -196,6 +202,8 @@ module.exports = {
               return li.collapsedSeries?.name || li.media.title
             }
           } else {
+            const mediaSortField = legacyMediaSortFields[payload.sortBy]
+            if (mediaSortField) return li.media[mediaSortField]
             return payload.sortBy.split('.').reduce((a, b) => a[b], li)
           }
         }
